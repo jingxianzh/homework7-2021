6bb20bdd1338f06e523f336f686bf5d6cae5b26d
jingxianzh <89872259+jingxianzh@users.noreply.github.com>
2021-11-18 23:55:01 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -9,6 +9,7 @@ window.addEventListener("load", function() {
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
 	video.play();
+	videoVolume.innerHTML = video.volume * 100 + "%";
 });
 
 document.querySelector("#pause").addEventListener("click", function(){
